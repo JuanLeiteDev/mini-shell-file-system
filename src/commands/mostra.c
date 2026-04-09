7b7abab22473a3286bebdf58312d7e302cc64f24
JuanLeiteDev <juanleite048@gmail.com>
2026-04-10 00:12:09 +0100

    Update mostra.c

--- src/commands/mostra.c
+++ src/commands/mostra.c
@@ -2,7 +2,7 @@
 #include <fcntl.h>
 #include <errno.h>
 
-#define TAMANHO 1024
+#define TAMANHO_BUFFER 1024
 
 int main(int argc, char *argv[]){
     if(argc != 2){
@@ -11,21 +11,23 @@ int main(int argc, char *argv[]){
     }
 
     int fd = open(argv[1], O_RDONLY);
-    if(errno == ENOENT){
-        write(STDERR_FILENO, "O ficheiro não existe.\n", 24);
-        return 1;
-    } else if(fd == -1){
-        write(STDERR_FILENO, "Erro ao tentar abrir ficheiro.\n", 32);
+    if(fd == -1){
+        if(errno == ENOENT){
+            write(STDERR_FILENO, "O ficheiro não existe.\n", 24);
+        } else {
+            write(STDERR_FILENO, "Erro ao tentar abrir ficheiro.\n", 32);
+        }
         return 1;
     }
-    char buffer[TAMANHO];
+
+    char buffer[TAMANHO_BUFFER];
     ssize_t bytes_lidos;
-    while((bytes_lidos = read(fd, buffer, TAMANHO)) > 0){
-        write(STDIN_FILENO, buffer, bytes_lidos);
+    while((bytes_lidos = read(fd, buffer, TAMANHO_BUFFER)) > 0){
+        write(STDOUT_FILENO, buffer, bytes_lidos);
     }
 
     if(bytes_lidos == -1){
-        write(STDERR_FILENO, "Erro ao tntar ler ficheiro.\n", 29);
+        write(STDERR_FILENO, "Erro ao tentar ler ficheiro.\n", 29);
         close(fd);
         return 1;
     }
