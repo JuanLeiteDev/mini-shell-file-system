20c548be0066fdc626de61ed29878eb618609d7a
JuanLeiteDev <juanleite048@gmail.com>
2026-04-16 18:31:26 +0100

    Acrescentei funções que podem ser utilizadas externamente pelos comandos

--- src/include/utils.c
+++ src/include/utils.c
@@ -1,19 +1,72 @@
 #include "utils.h"
+#include <stdio.h>
 
-void escrevaErro(const char msg[]){
+void escrevaErro(char msg[]){
     int len = 0;
     while(msg[len] != '\0') len++;
     write(STDERR_FILENO, msg, len);
 }
 
-int naoExiste(const int fd){
+char *concatenarString(char s1[], char s2[]){
+    int i = 0;
+    int j = 0;
+
+    while(s1[i] != '\0') i++;
+
+    s1[i] = ' ';
+    i++;
+
+    while(s2[j] != '\0'){
+        s1[i+j] = s2[j];
+        j++;
+    }
+
+    s1[i+j] = '\n';
+    s1[i+j+1] = '\0';
+
+    return s1;
+}
+
+int naoExiste(int fd, char nomeFicheiro[]){
     if(fd == -1){
         if(errno == ENOENT){
-            escrevaErro("O ficheiro não existe.\n");
+            char aviso[TAMANHO_BUFFER] = "O ficheiro não existe";
+            char *msg = concatenarString(aviso, nomeFicheiro);
+            escrevaErro(msg);
         } else {
-            escrevaErro("Erro ao abrir ficheiro.\n");
+            char aviso[TAMANHO_BUFFER] = "Erro ao abrir ficheiro";
+            char *msg = concatenarString(aviso, nomeFicheiro);
+            escrevaErro(msg);
         }
         return 1;
     }
     return 0;
+}
+
+void converteInt(int num, char *buffer, int *total_bytes){
+    if(num == 0){
+        buffer[0] = '0';
+        buffer[1] = ' ';
+        buffer[2] = '\0';
+        *total_bytes = 2;
+        return;
+    }
+    
+    int aux;
+    int tamanho = -1;
+    for(int i = 0; num != 0; i++){
+        aux = num % 10;
+        num = num / 10;
+        buffer[i] = aux + '0';
+        tamanho++;
+    }
+    *total_bytes = tamanho+2;
+    buffer[tamanho+1] = ' ';
+    buffer[tamanho+2] = '\0';
+    for(int j = 0; tamanho > 0; j++){
+        char temp = buffer[j];
+        buffer[j] = buffer[j+tamanho];
+        buffer[tamanho] = temp;
+        tamanho = tamanho - 2;
+    }
 }
--- src/include/utils.h
+++ src/include/utils.h
@@ -3,9 +3,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/stat.h>
 
 #define TAMANHO_BUFFER 1024
 
-void escrevaErro(const char msg[]);
+void escrevaErro(char msg[]);
 
-int naoExiste(const int fd);
+int naoExiste(int fd, char nomeFicheiro[]);
+
+char *concatenarString(char s1[], char s2[]);
+
+void converteInt(int num, char *buffer, int *total_bytes);
