8d1a0aa67ff3de642688f22a0092474ff9599e0d
JuanLeiteDev <juanleite048@gmail.com>
2026-04-17 15:12:15 +0100

    Comando informa funcional

--- src/commands/informa.c
+++ src/commands/informa.c
@@ -1,6 +1,106 @@
 #include "../include/utils.h"
 #include <stdio.h>
 
+int ehBissexto(int ano) {
+    if ((ano % 400) == 0) return 1;
+    if ((ano % 100) == 0) return 0;
+    if ((ano % 4) == 0) return 1;
+    return 0;
+}
+
+char *timestampParaString(unsigned long timestamp, char buffer[], size_t tamanhoBuffer) {
+    unsigned long dias;
+    int segundos;
+    int minutos;
+    int horas;
+    int dia;
+    int mes;
+    int ano;
+    int diasNoAno;
+    int i;
+
+    int diasMeses[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+
+    if (buffer == NULL) {
+        return NULL;
+    }
+
+    if (tamanhoBuffer < 20) {
+        if (tamanhoBuffer > 0) {
+            buffer[0] = '\0';
+        }
+        return NULL;
+    }
+
+    segundos = timestamp % 60;
+    timestamp /= 60;
+
+    minutos = timestamp % 60;
+    timestamp /= 60;
+
+    horas = timestamp % 24;
+    timestamp /= 24;
+
+    dias = timestamp;
+    ano = 1970;
+
+    while (1) {
+        diasNoAno = ehBissexto(ano) ? 366 : 365;
+
+        if (dias >= (unsigned long)diasNoAno) {
+            dias -= diasNoAno;
+            ano++;
+        } else {
+            break;
+        }
+    }
+
+    if (ehBissexto(ano)) {
+        diasMeses[1] = 29;
+    }
+
+    mes = 0;
+    while (mes < 12 && dias >= (unsigned long)diasMeses[mes]) {
+        dias -= diasMeses[mes];
+        mes++;
+    }
+
+    if (mes >= 12) {
+        buffer[0] = '\0';
+        return NULL;
+    }
+
+    dia = (int)dias + 1;
+    mes = mes + 1;
+
+    for (i = 0; i < (int)tamanhoBuffer; i++) {
+        buffer[i] = '\0';
+    }
+
+    buffer[0]  = (dia / 10) + '0';
+    buffer[1]  = (dia % 10) + '0';
+    buffer[2]  = '/';
+    buffer[3]  = (mes / 10) + '0';
+    buffer[4]  = (mes % 10) + '0';
+    buffer[5]  = '/';
+    buffer[6]  = ((ano / 1000) % 10) + '0';
+    buffer[7]  = ((ano / 100) % 10) + '0';
+    buffer[8]  = ((ano / 10) % 10) + '0';
+    buffer[9]  = (ano % 10) + '0';
+    buffer[10] = ' ';
+    buffer[11] = (horas / 10) + '0';
+    buffer[12] = (horas % 10) + '0';
+    buffer[13] = ':';
+    buffer[14] = (minutos / 10) + '0';
+    buffer[15] = (minutos % 10) + '0';
+    buffer[16] = ':';
+    buffer[17] = (segundos / 10) + '0';
+    buffer[18] = (segundos % 10) + '0';
+    buffer[19] = '\0';
+
+    return buffer;
+}
+
 int main(int argc, char *argv[]){
     if(argc != 2){
         escrevaErro("Uso: informa <ficheiro>\n");
@@ -17,10 +117,12 @@ int main(int argc, char *argv[]){
         return 1;
     }
 
+    // Obter Inode do arquivo
     unsigned long i_node = dados.st_ino;
     converteLong(i_node, aux, &tamanho);
     char permissoes[11];
 
+    // Analisar de que tipo é o arquivo
     if (S_ISDIR(dados.st_mode)) permissoes[0] = 'd';
     else if (S_ISREG(dados.st_mode)) permissoes[0] = '-';
     else if (S_ISLNK(dados.st_mode)) permissoes[0] = 'l';
@@ -29,6 +131,7 @@ int main(int argc, char *argv[]){
     else if (S_ISFIFO(dados.st_mode)) permissoes[0] = 'p';
     else if (S_ISSOCK(dados.st_mode)) permissoes[0] = 's';
     
+    // Verificar e guardar permissões
     permissoes[1] = (dados.st_mode & S_IRUSR) ? 'r' : '-';
     permissoes[2] = (dados.st_mode & S_IWUSR) ? 'w' : '-';
     permissoes[3] = (dados.st_mode & S_IXUSR) ? 'x' : '-';
@@ -40,32 +143,52 @@ int main(int argc, char *argv[]){
     permissoes[9] = (dados.st_mode & S_IXOTH) ? 'x' : '-';
     permissoes[10] = '\0';
 
+    // Concatena informações iniciais para a string final
     concatenarString(final, aux);
     concatenarString(final, permissoes);
 
+    // Quantidade de links para esse arquivo e concatena no final da string
     unsigned long nLinks = dados.st_nlink;
     converteLong(nLinks, aux, &tamanho);
     concatenarString(final, aux);
 
+    // Obtém o UID do usuário dono do arquivo
     unsigned int user_id = dados.st_uid;
     converteInt(user_id, aux, &tamanho);
 
+    // Abre um ficheiro onde contém as informações necessárias para obter o nome do dono atráves do UID
     int fd = open("/etc/passwd", O_RDONLY);
     if(fd != -1){
         char nome[100];
         nome[0] = '\0';
         if(procurarNomeUser(fd, aux, nome)){
             concatenarString(final, nome);
+            close(fd);
         } else {
             char msg[50] = "UID:";
             concatenarString(msg, aux);
             concatenarString(final, msg);
+            close(fd);
         }
     } else {
         char msg[50] = "UID:";
         concatenarString(msg, aux);
         concatenarString(final, msg);  
+        close(fd);
     }
-    printf("%s", final);
+
+    // Obtém tamanho do arquivo e concatena no final da string principal
+    unsigned long tamanho_arquivo = dados.st_size;
+    converteLong(tamanho_arquivo, aux, &tamanho);
+    concatenarString(final, aux);
+
+    char data[20];
+    if (timestampParaString((unsigned long)dados.st_ctime, data, sizeof(data)) != NULL) {
+        concatenarString(final, data);
+    }
+
+    concatenarString(final, argv[1]);
+    escreva(final);
+    escreva("\n");
     return 0;
 }
